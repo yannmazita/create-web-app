8143551494d8e0cdaaa5f8559d66ff5205e91a8e
Yann Mazita <dev.mazita@gmail.com>
2024-04-15 15:16:52 +0200

    Bug fixes

--- front/src/components/BaseServerInfo.vue
+++ front/src/components/BaseServerInfo.vue
@@ -2,20 +2,22 @@
     <div class="collapse collapse-arrow bg-base-200 rounded-none lg:col-start-10 lg:col-end-13">
         <input type="checkbox" />
         <div class="collapse-title text-white text-xl">
-            Server information (EU-West)
+            Server information
         </div>
         <div class="collapse-content text-white bg-base-300">
-            Online players: {{ activePlayers }}
+            Online users: {{ activePlayers }}
         </div>
     </div>
 </template>
 <script setup lang="ts">
 import { computed, watch } from 'vue';
 import { storeToRefs } from 'pinia';
+import { useAppStore } from '@/stores/app.ts';
 import { useClientStore } from '@/stores/client.ts';
 
 const clientStore = useClientStore();
-const { serverStats } = storeToRefs(gameStore);
+const appStore = useAppStore();
+const { serverStats } = storeToRefs(appStore);
 const { socketMessage } = storeToRefs(clientStore);
 
 const activePlayers = computed(() => {
@@ -24,7 +26,7 @@ const activePlayers = computed(() => {
 
 watch(socketMessage, (message) => {
     if (message.action === 'server_stats') {
-        gameStore.serverStats.active_users = message.data.active_users;
+        appStore.serverStats.active_users = message.data.active_users;
     }
 });
 </script>
