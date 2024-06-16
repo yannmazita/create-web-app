76948764e1ef8aaf57079bc6fffb8cbcfa7e45e4
Yann Mazita <dev.mazita@gmail.com>
2024-06-16 18:55:23 +0200

    SQLModel removal - Updated CRUD methods (WIP)

--- app/repository.py
+++ app/repository.py
@@ -2,6 +2,7 @@ import logging
 from typing import Generic, TypeVar
 from uuid import UUID
 
+from pydantic import BaseModel
 from sqlalchemy import (
     BooleanClauseList,
     func,
@@ -22,6 +23,7 @@ from sqlalchemy.exc import (
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Base
+from app.schemas import Base as BaseSchema
 
 logger = logging.getLogger(__name__)
 Model = TypeVar("Model", bound=Base)
@@ -40,7 +42,7 @@ class DatabaseRepository(Generic[Model]):
         self.model: type[Model] = model
         self.session: AsyncSession = session
 
-    async def create(self, data: dict) -> Model:
+    async def create(self, data: BaseSchema) -> Model:
         """
         Create a new instance of the model in the database.
 
@@ -51,15 +53,14 @@ class DatabaseRepository(Generic[Model]):
         """
         try:
             logger.debug(f"Creating {self.model.__name__}")
-            instance = self.model(**data)
-
+            instance = self.model(**data.model_dump())
             logger.debug(f"Adding {self.model.__name__} to session")
             self.session.add(instance)
             logger.debug("Committing session")
             await self.session.commit()
             logger.debug(f"Refreshing {self.model.__name__} instance")
             await self.session.refresh(instance)
-            logger.info(f"Created {self.model.__name__} with id {instance.id}")
+            logger.info(f"Created {self.model.__name__}")
             return instance
         except IntegrityError as e:
             logger.error("Integrity error occurred.", exc_info=False)
@@ -71,21 +72,22 @@ class DatabaseRepository(Generic[Model]):
             logger.error("Unexpected error occurred.", exc_info=False)
             raise e
 
-    async def get(self, id: UUID) -> Model:
+    async def get_by_attribute(self, value: UUID | str, column: str = "id") -> Model:
         """
         Get an instance of the model from the database.
 
         Args:
-            id: The id of the instance to be retrieved.
+            value: The value of the attribute to be used for filtering.
+            column: The column to be used for filtering.
         Returns:
             The retrieved instance.
         """
         try:
-            logger.debug(f"Getting {self.model.__name__} with id {id}")
-            query = select(self.model).where(self.model.id == id)
+            logger.debug(f"Getting {self.model.__name__} with {column} {value}")
+            query = select(self.model).where(getattr(self.model, column) == value)
             response = await self.session.execute(query)
             instance = response.scalar_one()
-            logger.info(f"Got {self.model.__name__} with id {id}")
+            logger.info(f"Got {self.model.__name__} with {column} {value}")
             return instance
         except MultipleResultsFound as e:
             logger.error("Multiple results found.", exc_info=False)
@@ -100,30 +102,38 @@ class DatabaseRepository(Generic[Model]):
             logger.error("Unexpected error occurred.", exc_info=False)
             raise e
 
-    async def update(self, id: UUID, data: dict) -> Model:
+    async def update_by_attribute(
+        self, data: BaseSchema, value: UUID | str, column: str = "id"
+    ) -> Model:
         """
         Update an instance of the model in the database.
 
         Args:
-            id: The id of the instance to be updated.
             data: The data to be used for updating the instance.
+            value: The value of the attribute to be used for filtering.
+            column: The column to be used for filtering.
         Returns:
             The updated instance.
         """
         try:
-            logger.debug(f"Updating {self.model.__name__} with id {id}")
-            instance = await self.get(id)
-            for key, value in data.items():
-                if key != "id":
-                    setattr(instance, key, value)
+            logger.debug(f"Updating {self.model.__name__} with {column} {value}")
+            instance = await self.get_by_attribute(value, column)
+
+            items = data.model_dump(exclude_unset=True).items()
+            for key, value in items:
+                # if key != "id":
+                setattr(instance, key, value)
             logger.debug(f"Adding {self.model.__name__} to session")
             self.session.add(instance)
             logger.debug("Committing session")
             await self.session.commit()
             logger.debug(f"Refreshing {self.model.__name__} instance")
             await self.session.refresh(instance)
-            logger.info(f"Updated {self.model.__name__} with id {id}")
+            logger.info(f"Updated {self.model.__name__} with {column} {value}")
             return instance
+        except MultipleResultsFound as e:
+            logger.error("Multiple results found.", exc_info=False)
+            raise e
         except NoResultFound as e:
             logger.error("No result found.", exc_info=False)
             raise e
@@ -137,16 +147,28 @@ class DatabaseRepository(Generic[Model]):
             logger.error("Unexpected error occurred.", exc_info=False)
             raise e
 
-    async def delete(self, id: UUID) -> Model:
+    async def delete(self, value: UUID | str, column: str = "id") -> Model:
+        """
+        Delete an instance of the model from the database.
+
+        Args:
+            value: The value of the attribute to be used for filtering.
+            column: The column to be used for filtering.
+        Returns:
+            The deleted instance.
+        """
         try:
-            logger.debug(f"Deleting {self.model.__name__} with id {id}")
-            instance = await self.get(id)
+            logger.debug(f"Deleting {self.model.__name__} with {column} {value}")
+            instance = await self.get_by_attribute(value, column)
             logger.debug(f"Deleting {self.model.__name__} from session")
             await self.session.delete(instance)
             logger.debug("Committing session")
             await self.session.commit()
-            logger.info(f"Deleted {self.model.__name__} with id {id}")
+            logger.info(f"Deleted {self.model.__name__} with {column} {value}")
             return instance
+        except MultipleResultsFound as e:
+            logger.error("Multiple results found.", exc_info=False)
+            raise e
         except NoResultFound as e:
             logger.error("No result found.", exc_info=False)
             raise e
@@ -191,64 +213,3 @@ class DatabaseRepository(Generic[Model]):
         except Exception as e:
             logger.error(f"Unexpected error occurred: {e}", exc_info=False)
             raise e
-
-    async def _filter_and_perform(
-        self,
-        operation,
-        expressions: list[BooleanClauseList] | None = None,
-        id: UUID | None = None,
-        data: dict | None = None,
-    ) -> None:
-        """
-        Filter instances of the model in the database and perform an operation.
-
-        This method is a helper method for performing operations on filtered instances.
-        Args:
-            operation: The operation to be performed.
-            expressions: Optional list of filter expressions.
-            id: Optional id of the instance to be filtered.
-            data: Optional dictionary containing data for updating instances.
-        """
-        query = operation(self.model)
-        if expressions:
-            query = query.where(*expressions)
-        if id:
-            query = query.where(self.model.id == id)
-        if data:
-            query = query.values(**data)
-
-        await self.session.execute(query)
-        await self.session.commit()
-
-    async def filter(
-        self,
-        *expressions: BooleanClauseList,
-        update_data: dict | None = None,
-        delete: bool = False,
-    ):
-        """
-        Filter instances of the model in the database.
-
-        Args:
-            *expressions: The filter expressions.
-            update_data: Optional dictionary containing data for updating instances.
-            delete: Boolean flag to indicate if the matched instances should be deleted.
-        Returns:
-            The list of matched instances.
-        """
-        query = select(self.model)
-        if expressions:
-            query = query.where(*expressions)
-
-        response = await self.session.execute(query)
-        instances = response.scalars().all()
-
-        if update_data:
-            await self._filter_and_perform(
-                sa_update, expressions=list(expressions), data=update_data
-            )
-
-        if delete:
-            await self._filter_and_perform(sa_delete, expressions=list(expressions))
-
-        return instances
